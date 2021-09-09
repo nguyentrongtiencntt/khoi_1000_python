092d26a1cb044ec0422a881f3d06391a94e6ea7b
NguyenTrongKhoi <nguyentrongkhoicntt@gmail.com>
2021-09-09 17:21:27 +0700

    bai19function

--- venv/Scripts/bai_18function.py
+++ venv/Scripts/bai_18function.py
@@ -1,10 +1,10 @@
 # Bài 18: Tính S(n) = 1 + x^2/2! + x^4/4! + … + x^2n/(2n)!
 def sumn(n):
-    s = 0
+    s = 1
     x = 2
     for i in range(1, n):
         s = s + pow(x, i) / (2 * i)
-        print("i = " + str(i) + " => " + "s + pow(x, i) / (2 * i)=" + str(s + pow(x, i) / (2 * i)))
+        print("i = " + str(i) + " => " + "s + pow(x, i) / (2 * i)=" + str(s))
         print("s = " + str(s))
     return s
 
